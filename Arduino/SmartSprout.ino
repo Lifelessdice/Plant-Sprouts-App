e025c8ddb9c81c0b2473a7a2e0e1cf306ad70593
Mirza Raian Ahmad <raian@student.chalmers.se>
2025-04-24 13:16:36 +0200

    #3 Implemented reading light intensity

--- Arduino/SmartSprout.ino
+++ Arduino/SmartSprout.ino
@@ -7,6 +7,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 
 void setup() {
+  pinMode(WIO_LIGHT, INPUT);
   Serial.begin(115200);
   dht.begin();
 }
@@ -16,12 +17,15 @@ void loop() {
 
     int temperature = dht.readTemperature();
     int humidity = dht.readHumidity();
+    int light = analogRead(WIO_LIGHT);
 
     // Print to Serial
     Serial.print("Temperature: ");
     Serial.println(temperature);
     Serial.print("Humidity: ");
     Serial.println(humidity);
+    Serial.print("Light value: ");
+    Serial.println(light);
     delay(10000);
   
 }
