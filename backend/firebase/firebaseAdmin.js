173c91a30dcd5b69a5ee1715d4138288ceba8366
Rahat Mir <rahatm@student.chalmers.se>
2025-05-15 20:00:47 +0200

    #49 Implemented admin connection to firebase from the backend.

---
+++ backend/firebase/firebaseAdmin.js
@@ -0,0 +1,11 @@
+const admin = require("firebase-admin");
+
+//The service account JSON is parsed from the environment variables in render
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
+//Inititalize the Firebase Admin SDK
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount);\,
+});
+
+module.exports = admin;
