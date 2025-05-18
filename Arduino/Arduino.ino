8b43c882f4460f5db5de2466f1df9e3d94bb8db9
Mirza Raian Ahmad <raian@student.chalmers.se>
2025-05-18 22:29:08 +0200

    #56 Minor refactoring

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -14,7 +14,7 @@ const int mqtt_port = 1883;
 // Sensor setup
 #define DHTPIN 2
 #define DHTTYPE DHT11
-#define SOIL_PIN A0
+#define SOIL_PIN A0  
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -22,43 +22,59 @@ DHT dht(DHTPIN, DHTTYPE);
 #define BLUE_LED 3
 #define RED_LED 4
 
+// MQTT client setup
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
+
+// LCD display
 TFT_eSPI tft;
 
 // Timing
 unsigned long lastMsgTime = 0;
 unsigned long lastAlertReceivedTime = 0;
-const unsigned long alertTimeout = 15000;
+const unsigned long alertTimeout = 30000; // 15 seconds of silence = app disconnected
 bool appWasSilent = false;
 String lastAlertMessage = "";
 
-// Face drawing
+// Smiley face drawing function
 void drawSmileyFace() {
-  tft.fillScreen(TFT_WHITE);
-  tft.fillCircle(160, 120, 60, TFT_YELLOW);
-  tft.drawCircle(160, 120, 60, TFT_BLACK);
-  tft.fillCircle(145, 105, 5, TFT_BLACK);
-  tft.fillCircle(175, 105, 5, TFT_BLACK);
+  tft.fillScreen(TFT_WHITE); // White background
+
+  // Face
+  tft.fillCircle(160, 120, 60, TFT_YELLOW); // Yellow face
+  tft.drawCircle(160, 120, 60, TFT_BLACK);  // Black outline
+
+  // Eyes
+  tft.fillCircle(145, 105, 5, TFT_BLACK); // Left eye
+  tft.fillCircle(175, 105, 5, TFT_BLACK); // Right eye
+
+  // Smile (inverted parabola)
   for (int x = -35; x <= 35; x++) {
     int y = -0.02 * x * x;
     tft.drawPixel(160 + x, 150 + y, TFT_BLACK);
   }
 }
 
+// Sad face drawing function
 void drawSadFace() {
-  tft.fillScreen(TFT_WHITE);
-  tft.fillCircle(160, 120, 60, TFT_CYAN);
-  tft.drawCircle(160, 120, 60, TFT_BLACK);
-  tft.fillCircle(145, 105, 5, TFT_BLACK);
-  tft.fillCircle(175, 105, 5, TFT_BLACK);
+  tft.fillScreen(TFT_WHITE); // White background
+
+  // Face
+  tft.fillCircle(160, 120, 60, TFT_CYAN); // Cyan face
+  tft.drawCircle(160, 120, 60, TFT_BLACK);  // Black outline
+
+  // Eyes
+  tft.fillCircle(145, 105, 5, TFT_BLACK); // Left eye
+  tft.fillCircle(175, 105, 5, TFT_BLACK); // Right eye
+
+  // Frown
   for (int x = -20; x <= 20; x++) {
-    int y = 0.05 * x * x;
+    int y = 0.05 * x * x; // Parabolic arc
     tft.drawPixel(160 + x, 140 + y, TFT_BLACK);
   }
 }
 
-// Comparison functions
+// Comparison Functions
 bool checkMoistureAndWarn(int soilMoisture) {
   if (soilMoisture < 40) {
     Serial.println("It's time to water your plant.");
@@ -99,7 +115,7 @@ bool checkHumidityAndWarn(int humidity) {
   }
 }
 
-// MQTT callback
+// MQTT Callback
 void callback(char *topic, byte *payload, unsigned int length) {
   String topicStr = String(topic);
   String message;
@@ -108,7 +124,7 @@ void callback(char *topic, byte *payload, unsigned int length) {
   }
 
   if (topicStr == "CROWmium/alert") {
-    lastAlertReceivedTime = millis();
+    lastAlertReceivedTime = millis(); // update the time of last received alert
     message.trim();
 
     if (appWasSilent) {
@@ -135,7 +151,7 @@ void callback(char *topic, byte *payload, unsigned int length) {
   }
 }
 
-// MQTT reconnect
+// MQTT Reconnect
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
@@ -156,10 +172,12 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
+  // LCD
   tft.begin();
   tft.setRotation(3);
-  drawSmileyFace();
+  drawSmileyFace();  // Default face
 
+  // WiFi Setup
   Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -170,15 +188,17 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // MQTT Setup
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
+  // Sensors and LEDs
   dht.begin();
   pinMode(WIO_LIGHT, INPUT);
   pinMode(SOIL_PIN, INPUT);
   pinMode(BLUE_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
-  digitalWrite(BLUE_LED, HIGH);
+  digitalWrite(BLUE_LED, HIGH); // Blue LED turned on by default
   digitalWrite(RED_LED, LOW);
 }
 
@@ -192,6 +212,7 @@ void loop() {
   unsigned long now = millis();
   const unsigned long publishInterval = 10000;
 
+  // Check if app is silent
   bool appIsSilent = (now - lastAlertReceivedTime > alertTimeout);
 
   if (appIsSilent && !appWasSilent) {
@@ -202,29 +223,36 @@ void loop() {
   if (now - lastMsgTime > publishInterval) {
     lastMsgTime = now;
 
+    // Read sensor values
     int temperature = dht.readTemperature();
     int humidity = dht.readHumidity();
     int light = analogRead(WIO_LIGHT);
     int soilRaw = analogRead(SOIL_PIN);
+
+    // Mapping soil moisture values to percentage
     int soilMoisture = map(soilRaw, 1023, 0, 0, 100);
     soilMoisture = constrain(soilMoisture, 0, 100);
 
+    // Convert values to strings
     char tempStr[10], humStr[10], lightStr[10], soilStr[10];
     snprintf(tempStr, sizeof(tempStr), "%d", temperature);
     snprintf(humStr, sizeof(humStr), "%d", humidity);
     snprintf(lightStr, sizeof(lightStr), "%d", light);
     snprintf(soilStr, sizeof(soilStr), "%d", soilMoisture);
 
+    // Print to Serial
     Serial.print("Temperature: "); Serial.println(tempStr);
     Serial.print("Humidity: "); Serial.println(humStr);
     Serial.print("Light: "); Serial.println(lightStr);
     Serial.print("Soil Moisture: "); Serial.println(soilStr);
 
+    // Publish to MQTT
     client.publish("CROWmium/rtl8720dn/temperature", tempStr);
     client.publish("CROWmium/rtl8720dn/humidity", humStr);
     client.publish("CROWmium/rtl8720dn/light", lightStr);
     client.publish("CROWmium/rtl8720dn/moisture", soilStr);
 
+    // Fallback to local control if app is silent
     if (appIsSilent) {
       bool warnMoisture = checkMoistureAndWarn(soilMoisture);
       bool warnLight = checkLightAndWarn(light);
@@ -233,6 +261,7 @@ void loop() {
 
       bool isWarning = warnMoisture || warnLight || warnTemp || warnHumidity;
 
+      // Show face and LED status
       if (isWarning) {
         digitalWrite(RED_LED, HIGH);
         digitalWrite(BLUE_LED, LOW);
