6a921d3b94763d564eb4f9f12a7b6ee23b724175
sumaya Omar <gussumom@student.gu.se>
2025-05-19 09:01:25 +0200

    #32 Fixed ProfileScreen and related files

--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,201 +1,289 @@
 import { Alert } from 'react-native';
-import { auth } from '../firebase'; 
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../firebase';
+import { createUserWithEmailAndPassword, sendEmailVerification,signInWithEmailAndPassword,sendPasswordResetEmail} from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
-import { db } from '../firebase';
 import React, { useState } from 'react';
 import { View, StyleSheet, Text,TextInput } from 'react-native';
 import CustomButton from '../components/CustomButton';
 import { Video } from 'expo-av';
 
 const LoginScreen = ({ navigation }) => {
-  const[email, setEmail]=useState('');
-  const[password, setPassword]=useState('');
-  const [showPassword, setShowPassword] = useState(false);
-  
-  const  userLogin = () => {
-    if (email.trim() === '') {
-      Alert.alert(' Enter your email');
-        return; }
-    
-      if (password.trim() === '') {
-        Alert.alert(' Enter your password');
-        return; }
-    
-        signInWithEmailAndPassword(auth, email, password)
-        .then(() => {
-          // No navigation here
-          console.log('Login successful');
-        })
-  .catch((error) => {
-    if (error.code === 'auth/user-not-found') {      
-      Alert.alert('Please create account. No account exists for this email');
-    } else if (error.code === 'auth/wrong-password') {
-      Alert.alert('Incorrect password');
-    }
-      else if (error.code === 'auth/invalid-credential') {
-        Alert.alert('Try again. The email or password is incorrect.');
-    } else if (error.code === 'auth/invalid-email') {
-        Alert.alert('Please enter a valid Email.');
-    }
-      else if (error.code === 'auth/weak-password') {
-        Alert.alert('Password should be at least 6 characters long.');
-    }
+const[email, setEmail]=useState('');
+const[password, setPassword]=useState('');
+const [username, setUsername] = useState('');
+const [showPassword, setShowPassword] = useState(false);
+const [showname, setname] = useState(false);
 
-     else {
-      Alert.alert(error.message);
-    }
-  });
+const forgotPassword  = async () => {
+if (!email.trim()){
+  Alert.alert('Enter your email');
+   return;
+ }
+ try { await sendPasswordResetEmail(auth, email);
+  Alert.alert('A reset link has been sent to your email');
+} catch (error) {
+  Alert.alert('error', error.message);
+}
 };
 
 
-    const Signup = async() => {
-        if (email.trim() === '') {
-          Alert.alert(' Enter your email');
-        return; }
-    
-      if (password.trim() === '') {
-        Alert.alert(' Enter your password');
-        return;}
-        try {
-    
-    const result = await createUserWithEmailAndPassword(auth, email, password);
-
-    
+const  userLogin = async () => {
+ if (email.trim() === '') {
+   Alert.alert(' Enter your email');
+     return; }
+    if (password.trim() === '') {
+     Alert.alert(' Enter your password');
+     return; }
+try {
+    const result = await signInWithEmailAndPassword(auth, email, password);
     const user = result.user;
 
-
-    await setDoc(doc(db, "accounts", user.uid), {
-          serEmail: user.email,
-          registeredTime: new Date().toISOString(),
-      });
-      
-    
-      Alert.alert('Your account is created successfully 🌱');
-    navigation.navigate('Home');
-  } 
+    await user.reload();
   
-  catch (error)  {
-    if (error.code === 'auth/email-already-in-use') {
-      Alert.alert('You already have an account, just log in');
-    } else if (error.code === 'auth/invalid-email') {
-      Alert.alert('Enter a valid Email');
-    } else if (error.code === 'auth/wrong-password') {
-        alert('Incorrect password.');
-    } else if (error.code === 'auth/user-not-found') {
-      Alert.alert('Please create account. No account exists for this email');
-    } else if (error.code === 'auth/invalid-credential') {
-      Alert.alert('Try again. The email or password is incorrect.');
-    } else {
-      Alert.alert(error.message);
-    }
-  }
+    if (!user.emailVerified) {
+        await sendEmailVerification(user);
+        Alert.alert('Email is not verified, a verification email has been sent')
+        return;
+      }
+
+
+
+
+       Alert.alert('Welcome' );
+
+
+
+
+       navigation.navigate('Home');
+  
+     }
+catch(error) {
+ if (error.code === 'auth/user-not-found') {   
+   Alert.alert('Please create account. No account exists for this email');
+ } else if (error.code === 'auth/wrong-password') {
+   Alert.alert('Incorrect password');
+ }
+   else if (error.code === 'auth/invalid-credential') {
+     Alert.alert('Try again. The email or password is incorrect.');
+ } else if (error.code === 'auth/invalid-email') {
+     Alert.alert('Please enter a valid Email.');
+ }
+  else {
+   Alert.alert(error.message);
+ }
+}
 };
-        
+
+
+ const Signup = async() => {
+   if (email.trim() === '') {
+       Alert.alert(' Enter your email');
+     return; }
+    if (password.trim() === '') {
+     Alert.alert(' Enter your password');
+     return;}
+  try {
+  const result = await createUserWithEmailAndPassword(auth, email, password);
+
+ const user = result.user;
+
+ await sendEmailVerification(user);
+
+
+ await setDoc(doc(db, "accounts", user.uid), {
+       email: user.email,
+       username: username,
+       registeredTime: new Date().toISOString(),
+   });
+ 
+   Alert.alert('Your account is created successfully 🌱');
+
+   Alert.alert('Please verify your email.A verification link has been sent');
+ 
+  
+   navigation.navigate('Login');
+}
+catch (error)  {
+ if (error.code === 'auth/email-already-in-use') {
+   Alert.alert('You already have an account, just log in');
+ } else if (error.code === 'auth/invalid-email') {
+   Alert.alert('Enter a valid Email');
+ } else if (error.code === 'auth/wrong-password') {
+     alert('Incorrect password.');
+ } else if (error.code === 'auth/user-not-found') {
+   Alert.alert('Please create account. No account exists for this email');
+ } else if (error.code === 'auth/invalid-credential') {
+   Alert.alert('Try again. The email or password is incorrect.');
+
+
+ } else if (error.code === 'auth/weak-password') {
+  Alert.alert('Password should be at least 6 characters');}
+  else {
+   Alert.alert(error.message);
+ }
+}
+};
+  
 
 return (
-  <View style={styles.wrapper}>
-    {/* Background video */}
-    <View style={styles.videoWrapper}>
-      <Video
-        source={require('../assets/animation3.mp4')}
-        style={styles.video}
-        isLooping
-        shouldPlay
-        isMuted
-        resizeMode="cover"
-      />
-    </View>
+<View style={styles.wrapper}>
+ {/* Background video */}
+ <View style={styles.videoWrapper}>
+   <Video
+     source={require('../assets/animation3.mp4')}
+     style={styles.video}
+     isLooping
+     shouldPlay
+     isMuted
+     resizeMode="cover"
+   />
+ </View>
 
-    {/* Login form */}
-    <View style={styles.page}>
-      <Text style={styles.header}>SmartSprout</Text>
 
-      <TextInput
-        style={styles.UserInput}
-        value={email}
-        placeholder="Enter your email"
-        onChangeText={setEmail}
-      />
 
-      <TextInput
-        style={styles.UserInput}
-        value={password}
-        placeholder="Enter your password"
-        onChangeText={setPassword}
-        secureTextEntry={!showPassword}
-      />
-      <Text style={styles.showPasswordstyle} onPress={() => setShowPassword(!showPassword)}>
-        {showPassword ? 'Hide password' : 'Show password'}
-      </Text>
+ {/* Login form */}
+ <View style={styles.page}>
+   <Text style={styles.header}>SmartSprout</Text>
 
-      <View style={styles.loginSignupStyle}>
-      <CustomButton title="Login" onPress={userLogin} style={{marginRight:15}}/>
-      <CustomButton title="Create account" onPress={Signup} />
-      </View>
-    </View>
-  </View>
+
+
+   <TextInput
+     style={styles.UserInput}
+     value={email}
+     placeholder="Enter your email"
+     onChangeText={setEmail}
+   />
+
+
+   <TextInput
+     style={styles.UserInput}
+     value={password}
+     placeholder="Enter your password"
+     onChangeText={setPassword}
+     secureTextEntry={!showPassword}
+   />
+   {showname &&
+   <TextInput
+   style={styles.UserInput}
+   value={username}
+   placeholder="Enter your name optional"
+   onChangeText={setUsername}
+  /> 
+ }
+
+
+   <Text style={styles.showPasswordstyle} onPress={() => setShowPassword(!showPassword)}>
+     {showPassword ? 'Hide password' : 'Show password'}
+   </Text>
+   <Text style={styles.ForgetPasswordstyle} onPress=  {forgotPassword}>
+   Forgot Password?
+   </Text>
+
+
+
+
+
+
+
+
+   <View style={styles.loginSignupStyle}>
+   <CustomButton title="Login" onPress={userLogin} style={{marginRight:15}}/>
+   <CustomButton title="Create account"  onPress = {() => {
+   if (!showname) {
+     setname(true);
+   } else {
+     Signup(); }
+   }} />
+   </View>
+ </View>
+</View>
 );
 };
 
+
+
 const styles = StyleSheet.create({
 wrapper: {
-  flex: 1,
-  backgroundColor: '#e6f3ec',
+flex: 1,
+backgroundColor: '#e6f3ec',
 },
 videoWrapper: {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '100%',
-  height: '100%',
-  zIndex: 0,
+position: 'absolute',
+top: 0,
+left: 0,
+width: '100%',
+height: '100%',
+zIndex: 0,
 },
 video: {
-  width: '100%',
-  height: '100%',
-  position: 'absolute',
+width: '100%',
+height: '100%',
+position: 'absolute',
 },
 page: {
-  flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
-  zIndex: 1,
-  paddingHorizontal: 20,
+flex: 1,
+alignItems: 'center',
+justifyContent: 'center',
+zIndex: 1,
+paddingHorizontal: 20,
 },
 header: {
-  fontSize: 40,
-  marginBottom: 10,
-  fontWeight: 'bold',
-  color: '#202b4a',
+fontSize: 40,
+marginBottom: 10,
+fontWeight: 'bold',
+color: '#202b4a',
 },
 UserInput: {
-  backgroundColor: '#fff',
-  width: '80%',
-  borderRadius: 10,
-  padding: 15,
-  borderWidth: 1,
-  marginBottom: 20,
-  borderColor: '#ccc',
+backgroundColor: '#fff',
+width: '80%',
+borderRadius: 10,
+padding: 15,
+borderWidth: 1,
+marginBottom: 20,
+borderColor: '#ccc',
 },
 loginSignupStyle: {
-  justifyContent: 'center',
-  flexDirection: 'row',
-  width: '75%',
-  alignItems: 'center',
-  marginTop: 20,
+justifyContent: 'center',
+flexDirection: 'row',
+width: '75%',
+alignItems: 'center',
+marginTop: 20,
 },
 
+
+
 showPasswordstyle: {
-  color: '#202b4a',
-  textAlign: 'right',
-  fontWeight: '400',
-  marginBottom: 9,
-  marginTop: -10,
-  width: '75%',
-  fontWeight: 'bold',
+color: '#202b4a',
+textAlign: 'left',
+fontWeight: '400',
+marginBottom: 9,
+marginTop: -10,
+width: '75%',
+fontWeight: 'bold',
+},
+ForgetPasswordstyle: {
+color: '#202b4a',
+textAlign: 'left',
+fontWeight: '400',
+marginBottom: 9,
+marginTop: -10,
+width: '75%',
+fontWeight: 'bold',
 },
 });
 
+
 export default LoginScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ frontend/screens/ProfileScreen.js
@@ -0,0 +1,395 @@
+import { auth , db} from '../firebase';
+import { signOut , updatePassword,updateEmail,sendEmailVerification, reauthenticateWithCredential, EmailAuthProvider, } from 'firebase/auth';
+import React, { useState,useEffect } from 'react';
+import{ View, Text, StyleSheet,TextInput,Alert }from 'react-native';
+import { Video } from 'expo-av';
+import CustomButton from '../components/CustomButton';
+import { doc, updateDoc, getDoc,setDoc } from 'firebase/firestore';
+import { TouchableOpacity } from 'react-native';
+import { ArrowLeft } from 'lucide-react-native';
+
+
+const ProfileScreen = ({ navigation }) => {
+const [editProfile, seteditProfile] = useState(false);
+const [showAccount, setAccount] = useState(false);
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [username, setUsername] = useState('');
+const [newPassword, setnewPassword] = useState('');
+const [oldEmail, setoldEmail] = useState('');
+
+
+
+const UserSignOut = () => {
+signOut(auth)
+  .catch((error) => {
+   Alert.alert('logout failed.');
+ });
+};
+
+
+
+
+useEffect(() => {
+const getUserinformation = async () => {
+const user = auth.currentUser;
+await user.reload()
+if (user) {
+setEmail(user.email);
+setoldEmail(user.email);
+const userData = doc(db, 'accounts', user.uid);
+const data  = await getDoc(userData);
+if (data.exists()) {
+const dataInformaton = data.data();
+setUsername(dataInformaton.username || '');
+}
+} else {
+Alert.alert('Not Logged in');
+}
+};
+
+
+
+getUserinformation();
+}, []);
+
+
+const  changeUserInfo = async () => {
+const user = auth.currentUser;
+
+
+
+if (!user) {
+   Alert.alert('Not Logged in');
+   return;
+ }
+if (password.trim() === '') {
+   Alert.alert('Enter your current password');
+   return;
+}
+
+
+
+try {
+const userCredential = EmailAuthProvider.credential(user.email, password);
+await reauthenticateWithCredential(user, userCredential);
+
+
+
+await user.reload();
+
+
+if (!user.emailVerified) {
+await sendEmailVerification(user);
+setEmail(oldEmail);
+Alert.alert('Please verify your current email before changing. A verification link has been sent.');
+return;
+}
+
+
+
+
+if (newPassword.trim() !== '') {
+await updatePassword(user, newPassword);
+Alert.alert('Password updated successfully 🌱');
+setnewPassword('');
+   }
+
+
+const userInformation = doc(db, 'accounts', user.uid);
+const currentData = await getDoc(userInformation);
+if (!currentData.exists()) {
+ await setDoc(userInformation, {
+   username: username,
+   email: user.email,
+   registeredTime: new Date().toISOString(),
+ });
+ Alert.alert('You changed your username successfully🌱');
+}
+ else {
+ const currentName = currentData.data().username || '';
+ if (username.trim() !== '' && username !== currentName) {
+   await updateDoc(userInformation, { username: username });
+   Alert.alert('You changed your username successfully 🌱');
+}
+
+
+}
+
+
+setAccount(false);
+seteditProfile(false);
+setPassword('');
+
+
+ } catch (error) {
+    if (error.code === 'auth/invalid-credential') {
+     Alert.alert('Incorrect Password', 'Please try again.');
+} else {
+Alert.alert('Error', error.message);
+
+
+
+
+ }
+}
+};
+return (
+     <View style={styles.wrapper}>
+       {/* arrow return button */}
+       <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButtonArrow}>
+      <ArrowLeft color="#1e3a8a" size={24} />
+       </TouchableOpacity>
+     {/* Background video */}
+     <View style={styles.videoWrapper}>
+       <Video
+         source={require('../assets/animation3.mp4')}
+         style={styles.video}
+         isLooping
+         shouldPlay
+         isMuted
+         resizeMode="cover"
+         />
+</View>
+
+
+
+
+{/* Profile */}
+<View style={styles.page}>
+<Text style={styles.header}>SmartSprout 🌱</Text>
+{/* Settings button */}
+
+
+{!editProfile &&!showAccount&&(
+<View style={styles.spaceBetween}>
+ <CustomButton
+ title="Settings ⚙️"
+ onPress={() => {
+  setAccount(true);
+  seteditProfile(false);
+}}
+/>
+ <CustomButton
+ title="Logout"
+ onPress={UserSignOut}
+/>
+</View>
+)}
+
+
+{showAccount && ! editProfile && (
+<View style={styles.middleView}>
+<Text style = {styles.text}>Your account information</Text>
+<Text style={styles.textStyle}>Email: {email}</Text>
+<Text style={styles.textStyle}>Username: {username}</Text>
+  
+
+
+<CustomButton
+ title="Change Profile"
+ onPress={() => {
+   seteditProfile(true);
+}}
+/>
+</View>
+)} 
+ 
+{ editProfile   && (
+<View style={ styles.middleView}>
+       <TextInput
+         placeholder="Your new Username"
+         style={styles.input}
+         value={username}
+         onChangeText={setUsername}
+
+
+
+
+       />
+    
+   
+       <TextInput
+         placeholder="Your new password optional:"
+         style={styles.input}
+         value={newPassword}
+         onChangeText={setnewPassword}
+     
+       />
+        <TextInput
+         placeholder="Enter your current password"
+         style={styles.input}
+         value={password}
+         onChangeText={setPassword}
+       />
+       <CustomButton title="Save" onPress={changeUserInfo} />
+       </View>
+       )}
+       </View>
+       </View>
+   );
+};
+
+
+const styles = StyleSheet.create({
+wrapper: {
+ flex: 1,
+ backgroundColor: '#e6f3ec',
+},
+middleView: {
+alignItems: 'center',
+paddingHorizontal: 20,
+width: '100%',
+ },
+
+
+
+
+videoWrapper: {
+   position: 'absolute',
+   top: 0,
+   left: 0,
+   width: '100%',
+   height: '100%',
+   zIndex: 0,
+ },
+ video: {
+   width: '100%',
+   height: '100%',
+   position: 'absolute',
+ },
+page: {
+   flex: 1,
+   alignItems: 'center',
+   justifyContent: 'center',
+   zIndex: 1,
+   paddingHorizontal: 20,
+ },
+ header: {
+   fontSize: 40,
+   marginBottom: 10,
+   fontWeight: 'bold',
+},
+setting: {
+   fontSize: 20,
+   color: 'black',
+   marginRight: 40,
+   fontWeight: 'bold',
+ },
+input: {
+   backgroundColor: '#fff',
+   width: '80%',
+   borderRadius: 10,
+   padding: 15,
+   borderWidth: 1,
+   marginBottom: 20,
+   borderColor: '#ccc',
+ },
+
+
+ spaceBetween : {
+   justifyContent:'space-between',
+   height: 130,
+   alignItems: 'center',
+   flexDirection: 'row',
+   width: '70%',
+   marginTop: 20,
+ },
+
+
+
+
+ textStyle: {
+   backgroundColor: '#fff',
+   width: '80%',
+   borderRadius: 10,
+   padding: 15,
+   borderWidth: 1,
+   marginBottom: 20,
+   borderColor: '#ccc',
+ },
+ text : {
+   fontSize: 20,
+   color: 'black',
+   marginRight: 40,
+   fontWeight: 'bold',
+ },
+
+
+
+
+ backButtonArrow :{
+  backgroundColor: '#fff',
+  position: 'absolute',
+  padding: 15,
+  top: 40,
+  left: 25,
+  borderRadius: 15,
+  zIndex: 25,
+ },
+});
+
+
+
+export default ProfileScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
