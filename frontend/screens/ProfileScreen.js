5a451708769d470d675e16fe9287d1da71c9b72a
sumaya Omar <gussumom@student.gu.se>
2025-05-20 17:40:54 +0200

    #32 Refactored ProfileScreen to use TopBar component for back arrow

--- frontend/screens/ProfileScreen.js
+++ frontend/screens/ProfileScreen.js
@@ -6,8 +6,7 @@ import { Video } from 'expo-av';
 import CustomButton from '../components/CustomButton';
 import { doc, updateDoc, getDoc,setDoc } from 'firebase/firestore';
 import { TouchableOpacity } from 'react-native';
-import { ArrowLeft } from 'lucide-react-native';
-
+import TopBar from '../components/TopBar';
 
 
 const ProfileScreen = ({ navigation }) => {
@@ -20,15 +19,12 @@ const [newPassword, setnewPassword] = useState('');
 const [oldEmail, setoldEmail] = useState('');
 
 
-
 const UserSignOut = () => {
 signOut(auth)
-  .catch((error) => {
-   Alert.alert('logout failed.');
- });
+ .catch((error) => {
+  Alert.alert('logout failed.');
+});
 };
-
-
 
 
 useEffect(() => {
@@ -50,25 +46,24 @@ Alert.alert('Not Logged in');
 };
 
 
-
 getUserinformation();
 }, []);
+
+
 
 
 const  changeUserInfo = async () => {
 const user = auth.currentUser;
 
 
-
 if (!user) {
-   Alert.alert('Not Logged in');
-   return;
- }
-if (password.trim() === '') {
-   Alert.alert('Enter your current password');
-   return;
+  Alert.alert('Not Logged in');
+  return;
 }
-
+if (password.trim() === '') {
+  Alert.alert('Enter your current password');
+  return;
+}
 
 
 try {
@@ -76,9 +71,7 @@ const userCredential = EmailAuthProvider.credential(user.email, password);
 await reauthenticateWithCredential(user, userCredential);
 
 
-
 await user.reload();
-
 
 if (!user.emailVerified) {
 await sendEmailVerification(user);
@@ -88,72 +81,65 @@ return;
 }
 
 
-
-
 if (newPassword.trim() !== '') {
 await updatePassword(user, newPassword);
 Alert.alert('Password updated successfully 🌱');
 setnewPassword('');
-   }
+  }
 
 
 const userInformation = doc(db, 'accounts', user.uid);
 const currentData = await getDoc(userInformation);
 if (!currentData.exists()) {
- await setDoc(userInformation, {
-   username: username,
-   email: user.email,
-   registeredTime: new Date().toISOString(),
- });
- Alert.alert('You changed your username successfully🌱');
+await setDoc(userInformation, {
+  username: username,
+  email: user.email,
+  registeredTime: new Date().toISOString(),
+});
+Alert.alert('You changed your username successfully🌱');
 }
- else {
- const currentName = currentData.data().username || '';
- if (username.trim() !== '' && username !== currentName) {
-   await updateDoc(userInformation, { username: username });
-   Alert.alert('You changed your username successfully 🌱');
-}
-
-
+else {
+const currentName = currentData.data().username || '';
+if (username.trim() !== '' && username !== currentName) {
+  await updateDoc(userInformation, { username: username });
+  Alert.alert('You changed your username successfully 🌱');
 }
 
+}
 
 setAccount(false);
 seteditProfile(false);
 setPassword('');
 
 
- } catch (error) {
-    if (error.code === 'auth/invalid-credential') {
-     Alert.alert('Incorrect Password', 'Please try again.');
+
+} catch (error) {
+   if (error.code === 'auth/invalid-credential') {
+    Alert.alert('Incorrect Password', 'Please try again.');
 } else {
 Alert.alert('Error', error.message);
 
 
-
-
- }
+}
 }
 };
 return (
-     <View style={styles.wrapper}>
-       {/* arrow return button */}
-       <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButtonArrow}>
-      <ArrowLeft color="#1e3a8a" size={24} />
-       </TouchableOpacity>
-     {/* Background video */}
-     <View style={styles.videoWrapper}>
-       <Video
-         source={require('../assets/animation3.mp4')}
-         style={styles.video}
-         isLooping
-         shouldPlay
-         isMuted
-         resizeMode="cover"
-         />
+    <View style={styles.wrapper}>
+      {/* arrow return button */}
+      <TopBar 
+      title="" 
+      onBackPress={() => navigation.goBack()} />
+    {/* Background video */}
+    <View style={styles.videoWrapper}>
+      <Video
+        source={require('../assets/animation3.mp4')}
+        style={styles.video}
+        isLooping
+        shouldPlay
+        isMuted
+        resizeMode="cover"
+        />
 </View>
-
-
 
 
 {/* Profile */}
@@ -161,19 +147,18 @@ return (
 <Text style={styles.header}>SmartSprout 🌱</Text>
 {/* Settings button */}
 
-
 {!editProfile &&!showAccount&&(
 <View style={styles.spaceBetween}>
- <CustomButton
- title="Settings ⚙️"
- onPress={() => {
-  setAccount(true);
-  seteditProfile(false);
+<CustomButton
+title="Settings ⚙️"
+onPress={() => {
+ setAccount(true);
+ seteditProfile(false);
 }}
 />
- <CustomButton
- title="Logout"
- onPress={UserSignOut}
+<CustomButton
+title="Logout"
+onPress={UserSignOut}
 />
 </View>
 )}
@@ -184,155 +169,231 @@ return (
 <Text style = {styles.text}>Your account information</Text>
 <Text style={styles.textStyle}>Email: {email}</Text>
 <Text style={styles.textStyle}>Username: {username}</Text>
-  
+
+
 
 
 <CustomButton
- title="Change Profile"
- onPress={() => {
-   seteditProfile(true);
+title="Change Profile"
+onPress={() => {
+  seteditProfile(true);
 }}
 />
 </View>
-)} 
- 
+)}
 { editProfile   && (
 <View style={ styles.middleView}>
-       <TextInput
-         placeholder="Your new Username"
-         style={styles.input}
-         value={username}
-         onChangeText={setUsername}
+      <TextInput
+        placeholder="Your new Username"
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername}
 
 
 
 
-       />
-    
+
+
+
+
+      />
+  
+ 
+      <TextInput
+        placeholder="Your new password optional:"
+        style={styles.input}
+        value={newPassword}
+        onChangeText={setnewPassword}
    
+      />
        <TextInput
-         placeholder="Your new password optional:"
-         style={styles.input}
-         value={newPassword}
-         onChangeText={setnewPassword}
-     
-       />
-        <TextInput
-         placeholder="Enter your current password"
-         style={styles.input}
-         value={password}
-         onChangeText={setPassword}
-       />
-       <CustomButton title="Save" onPress={changeUserInfo} />
-       </View>
-       )}
-       </View>
-       </View>
-   );
+        placeholder="Enter your current password"
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <CustomButton title="Save" onPress={changeUserInfo} />
+      </View>
+      )}
+      </View>
+      </View>
+  );
 };
+
+
 
 
 const styles = StyleSheet.create({
 wrapper: {
- flex: 1,
- backgroundColor: '#e6f3ec',
+flex: 1,
+backgroundColor: '#e6f3ec',
 },
 middleView: {
 alignItems: 'center',
 paddingHorizontal: 20,
 width: '100%',
- },
-
-
-
+},
 
 videoWrapper: {
-   position: 'absolute',
-   top: 0,
-   left: 0,
-   width: '100%',
-   height: '100%',
-   zIndex: 0,
- },
- video: {
-   width: '100%',
-   height: '100%',
-   position: 'absolute',
- },
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: 0,
+},
+video: {
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+},
 page: {
-   flex: 1,
-   alignItems: 'center',
-   justifyContent: 'center',
-   zIndex: 1,
-   paddingHorizontal: 20,
- },
- header: {
-   fontSize: 40,
-   marginBottom: 10,
-   fontWeight: 'bold',
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  zIndex: 1,
+  paddingHorizontal: 20,
+},
+header: {
+  fontSize: 40,
+  marginBottom: 10,
+  fontWeight: 'bold',
 },
 setting: {
-   fontSize: 20,
-   color: 'black',
-   marginRight: 40,
-   fontWeight: 'bold',
- },
+  fontSize: 20,
+  color: 'black',
+  marginRight: 40,
+  fontWeight: 'bold',
+},
 input: {
-   backgroundColor: '#fff',
-   width: '80%',
-   borderRadius: 10,
-   padding: 15,
-   borderWidth: 1,
-   marginBottom: 20,
-   borderColor: '#ccc',
- },
-
-
- spaceBetween : {
-   justifyContent:'space-between',
-   height: 130,
-   alignItems: 'center',
-   flexDirection: 'row',
-   width: '70%',
-   marginTop: 20,
- },
-
-
-
-
- textStyle: {
-   backgroundColor: '#fff',
-   width: '80%',
-   borderRadius: 10,
-   padding: 15,
-   borderWidth: 1,
-   marginBottom: 20,
-   borderColor: '#ccc',
- },
- text : {
-   fontSize: 20,
-   color: 'black',
-   marginRight: 40,
-   fontWeight: 'bold',
- },
-
-
-
-
- backButtonArrow :{
   backgroundColor: '#fff',
-  position: 'absolute',
+  width: '80%',
+  borderRadius: 10,
   padding: 15,
-  top: 40,
-  left: 25,
-  borderRadius: 15,
-  zIndex: 25,
- },
+  borderWidth: 1,
+  marginBottom: 20,
+  borderColor: '#ccc',
+},
+
+
+
+
+spaceBetween : {
+  justifyContent:'space-between',
+  height: 130,
+  alignItems: 'center',
+  flexDirection: 'row',
+  width: '70%',
+  marginTop: 20,
+},
+
+
+
+
+
+
+
+
+textStyle: {
+  backgroundColor: '#fff',
+  width: '80%',
+  borderRadius: 10,
+  padding: 15,
+  borderWidth: 1,
+  marginBottom: 20,
+  borderColor: '#ccc',
+},
+text : {
+  fontSize: 20,
+  color: 'black',
+  marginRight: 40,
+  fontWeight: 'bold',
+},
+
+
+
+
+
+
+
+
+backButtonArrow :{
+ backgroundColor: '#fff',
+ position: 'absolute',
+ padding: 15,
+ top: 40,
+ left: 25,
+ borderRadius: 15,
+ zIndex: 25,
+},
 });
 
 
-
 export default ProfileScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
